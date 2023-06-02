92f0fff438000e97dea1d803ec3d06709e2d1dc1
Lassi Chen <lachen@utu.fi>
2023-06-02 21:02:41 +0300

    git init and working board

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chess
+Chess made on Python
---
+++ game.py
@@ -0,0 +1,8 @@
+board = []
+for i in range(8):
+    board.append([])
+    for j in range(8):
+        board[i].append(0)
+
+for i in range(8):
+    print(board[i])
