a95d22175775ea557dd73c70b415859ec7427e20
Lassi Chen <lachen@utu.fi>
2023-06-05 14:10:25 +0300

    simplified rook function

--- components/pieces.py
+++ components/pieces.py
@@ -1,9 +1,4 @@
 def rook(s_coord, e_coord):
-    for i in range(8):
-        s_coord_test1 = (i, s_coord[1])
-        s_coord_test2 = (s_coord[0], i)
-
-        if s_coord_test1 == e_coord or s_coord_test2 == e_coord:
-            return True
-
+    if s_coord[0] == e_coord[0] or s_coord[1] == e_coord[1]:
+        return True
     return False
