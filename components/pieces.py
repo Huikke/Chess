992682e5925159cbb16bcca9a0a4192de5f713a9
Lassi Chen <lachen@utu.fi>
2023-06-06 17:10:09 +0300

    Added queens

--- components/pieces.py
+++ components/pieces.py
@@ -2,6 +2,7 @@ class Piece:
     def __init__(self, color, position):
         self.color = color
         self.position = position
+        self.letter = "?" # Gets replaced in subclass
     
     def __str__(self):
         if self.color == "W":
@@ -30,6 +31,20 @@ class Bishop(Piece):
         col_distance = abs(coord[1] - self.position[1])
         
         if row_distance == col_distance:
+            self.position = coord
+            return True
+        return False
+
+class Queen(Piece):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+        self.letter = "Q"
+
+    def movement(self, coord):
+        row_distance = abs(coord[0] - self.position[0])
+        col_distance = abs(coord[1] - self.position[1])
+        
+        if self.position[0] == coord[0] or self.position[1] == coord[1] or row_distance == col_distance:
             self.position = coord
             return True
         return False
