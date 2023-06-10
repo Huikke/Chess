667be817ea1e07a02a4e124488c24c69f1e23272
Lassi Chen <lachen@utu.fi>
2023-06-10 04:14:46 +0300

    Now prints message if invalid input

--- components/input_converter.py
+++ components/input_converter.py
@@ -1,8 +1,13 @@
 # Converts an input to a form python understands
-def input_converter(move: str) -> int:
+def input_converter(move):
     # Converts latter part of coord
     col = ord(move[0].upper()) - 65
+    if col < 65 and col > 72:
+        return ValueError
+
     # Converts number part of coord
+    if not move[1].isdigit() or int(move[1]) < 1 or int(move[1]) > 8:
+        return ValueError
     row = int(move[1]) - 1
     row = abs(row - 7)
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import components.board as board
 from components.input_converter import input_converter
-import components.pieces as pieces
 from components.game import game_turn
 
 board_state = board.generate()
@@ -10,6 +9,9 @@ while True:
     move = input("Move piece: ") # Start coord and end coord like E1E7
     str_coord = input_converter(move[:2])
     dest_coord = input_converter(move[2:])
+    if str_coord == ValueError or dest_coord == ValueError:
+        print("Invalid input!")
+        continue
 
     game_move = game_turn(board_state, str_coord, dest_coord)
 
