0aba8ddc26252173ed778b72021e03429782e5b7
Lassi Chen <lachen@utu.fi>
2023-06-13 08:43:02 +0300

    Add checkmate testing in progress

--- components/game.py
+++ components/game.py
@@ -38,7 +38,6 @@ class Chess():
 
     def __init__(self):
         self.board_pieces = list(self.starting_pieces)
-        # self.infinite = False
         self.turn = "W" #TODO
         self.castling = ["K", "Q", "k", "q"] #TODO
         self.en_passant = "-"
@@ -48,18 +47,16 @@ class Chess():
     def state(self):
         return self.board_pieces
 
-    def move(self, str_coord, dest_coord):
+    def move(self, str_coord, dest_coord, testing=False):
         en_passant_check = self.en_passant
-        self.captured_piece = None
-        # if self.infinite == False and self.check_check() == True:
-        #     self.infinite == True
-        #     for piece in self.board_pieces:
-        #         if piece.color == self.turn:
-        #             for x in range(8):
-        #                 for y in range(8):
-        #                     if self.move(piece.position, (x, y)) == True:
-
-        #     self.infinite == False
+        # if testing == False:
+        #     if self.check_check() == True:
+        #         for piece in self.board_pieces:
+        #             if piece.color == self.turn:
+        #                 for x in range(8):
+        #                     for y in range(8):
+        #                         if self.move(piece.position, (x, y), True) == True:
+        #                             pass
 
         for piece in self.board_pieces:
             if piece.position == str_coord:
@@ -87,36 +84,42 @@ class Chess():
                         self.board_pieces.append(destination_check)
                     return "Check"
 
-                if isinstance(piece, pieces.Pawn):
-                    # En passant
-                    if abs(row_distance) == 2:
-                        move_direction = 1 if piece.color == "W" else -1
-                        self.en_passant = (dest_coord[0] + move_direction, dest_coord[1])
-                    # Promotion
-                    if dest_coord[0] == 0 or dest_coord[0] == 7:
-                        while True:
-                            promote_to = input("Choose what the pawn promote to: ") # One letter
-                            if promote_to == "Q" or promote_to == "q":
-                                self.board_pieces.append(pieces.Queen(piece.color, dest_coord))
-                            elif promote_to == "R" or promote_to == "r":
-                                self.board_pieces.append(pieces.Rook(piece.color, dest_coord))
-                            elif promote_to == "N" or promote_to == "n":
-                                self.board_pieces.append(pieces.Knight(piece.color, dest_coord))
-                            elif promote_to == "B" or promote_to == "b":
-                                self.board_pieces.append(pieces.Bishop(piece.color, dest_coord))
-                            else:
-                                print("Invalid input!")
-                                continue
-                            # Remove the pawn
-                            self.board_pieces.remove(piece)
-                            break
-                    # Disable double move
-                    piece.first_move = False
+                if testing == False:
+                    if isinstance(piece, pieces.Pawn):
+                        # En passant
+                        if abs(row_distance) == 2:
+                            move_direction = 1 if piece.color == "W" else -1
+                            self.en_passant = (dest_coord[0] + move_direction, dest_coord[1])
+                        # Promotion
+                        if dest_coord[0] == 0 or dest_coord[0] == 7:
+                            while True:
+                                promote_to = input("Choose what the pawn promote to: ") # One letter
+                                if promote_to == "Q" or promote_to == "q":
+                                    self.board_pieces.append(pieces.Queen(piece.color, dest_coord))
+                                elif promote_to == "R" or promote_to == "r":
+                                    self.board_pieces.append(pieces.Rook(piece.color, dest_coord))
+                                elif promote_to == "N" or promote_to == "n":
+                                    self.board_pieces.append(pieces.Knight(piece.color, dest_coord))
+                                elif promote_to == "B" or promote_to == "b":
+                                    self.board_pieces.append(pieces.Bishop(piece.color, dest_coord))
+                                else:
+                                    print("Invalid input!")
+                                    continue
+                                # Remove the pawn
+                                self.board_pieces.remove(piece)
+                                break
+                        # Disable double move
+                        piece.first_move = False
 
-                # Set en passant back to "-" if it wasn't set up this turn
-                if self.en_passant == en_passant_check:
-                    self.en_passant = "-"
-                self.turn = "B" if self.turn == "W" else "W"
+                    # Set en passant back to "-" if it wasn't set up this turn
+                    if self.en_passant == en_passant_check:
+                        self.en_passant = "-"
+                    self.turn = "B" if self.turn == "W" else "W"
+                else: # Revert the changes
+                    piece.position = str_coord
+                    if destination_check != None:
+                        self.board_pieces.append(destination_check)
+
                 return True
         return False
 
